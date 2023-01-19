2ee471538e43ced59f56e8bc65d721d83b9dcfaf
fschmid <fschmid@student.42heilbronn.de>
2023-01-19 10:42:29 +0100

    unbind :Ex

--- lua/fschmid/remap.lua
+++ lua/fschmid/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("i", "jk", "<ESC>")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
 
