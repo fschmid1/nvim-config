08be0caa081df51f9fc202915194fecc4010c8ab
Felix Schmid <felix.schmid@festech.de>
2023-01-27 18:18:59 +0100

    fix prettier

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -3,7 +3,7 @@ local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 
 local sources = {
-	formatting.prettierd.with {
+	formatting.prettier.with {
 		filetypes = { "javascript", "html", "json", "svelte", "css", "javascriptreact", "typescript", "typescriptreact" },
 		extra_filetypes = { "svelte" }
 	},
