78f501930bbd876cf9de3a45f322276bc4b1c5c1
Felix <felix.schmid@festech.de>
2024-01-07 15:34:50 +0100

    removed old config

--- after/plugin/telescope.lua
+++
@@ -1,8 +0,0 @@
-local telescope = require('telescope')
-local builtin = require('telescope.builtin')
-
-telescope.load_extension("live_grep_args")
-
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true })
