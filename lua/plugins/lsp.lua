1a9deedef74692be06e86cae7d8f4f40101bebb7
fsc <fsc@cirosec.de>
2024-01-23 15:46:30 +0100

    fix lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        html = {
+          filetypes = {
+            "html",
+            "templ",
+          },
+        },
+        tailwindcss = {
+          filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+          init_options = { userLanguages = { templ = "html" } },
+        },
+        htmx = {
+          filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+        },
+      },
+    },
+  },
+}
