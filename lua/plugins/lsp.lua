8efc86f3fd41d4aeba8c05199ee2b44037b8450b
fsc <fsc@cirosec.de>
2024-01-29 10:10:08 +0100

    tailwind

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,7 +12,7 @@ return {
           },
         },
         tailwindcss = {
-          filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+          filetypes = { "templ", "astro", "javascript", "typescript", "react", "html" },
           init_options = { userLanguages = { templ = "html" } },
         },
         htmx = {
